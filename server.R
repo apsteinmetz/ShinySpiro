1f97f4b127963ae78980f1dd3fe0d7f7f5cf27c9
apsteinmetz <apsteinmetz@yahoo.com>
2015-02-27 16:40:45 -0500

    version 1.0

--- server.R
+++ server.R
@@ -4,24 +4,24 @@
 #
 # http://shiny.rstudio.com
 #
+# Spirograph
+# author: Art Steinmetz
 
 library(shiny)
+library(grid)
 
 shinyServer(function(input, output) {
-  #http://www.r-bloggers.com/ggplot2-version-of-figures-in-%E2%80%9Clattice-multivariate-data-visualization-with-r%E2%80%9D-part-12/
   
-  library(plotrix)
   library(ggplot2)
-  library(shape)
   
-  debug=TRUE
-  # Spirograph
-  
+  debug=FALSE
+
   # angle in radians
   # draw 2*pi cycles with a point every 1/density radians
   calcCycles=100
   density =100
   defaultTheta <- 2 * pi * seq(0, calcCycles, by = 1/density)
+  defaultPoints = length(defaultTheta)
   
 circle <- function(cx=0,cy=0,radius = 1, npoints = 100){
     tt <- seq(0,2*pi,length.out = npoints)
@@ -29,131 +29,140 @@ circle <- function(cx=0,cy=0,radius = 1, npoints = 100){
     yy <- cy + radius * sin(tt)
     return(data.frame(x = xx, y = yy))
 }  
-ggoverlayCircles<-function(radius1,radius2,penLoc){
-    #uses Plotrix
-    c1<-ggplot(data=circle(0,0,radius1),aes(x,y))+geom_path()
-    c2<-ggplot(data=circle(radius1+radius2,0,abs(radius2)),aes(x,y))+geom_path()
-    # l1<- c(x=c(radius1+radius2,radius1+radius2-penLoc*sign(radius2)),y=c(0,0))
-    l1<- geom_segment(aes(x = radius1+radius2, y = 0, xend = radius1+radius2-penLoc*sign(radius2), yend = 0), arrow = arrow(length = unit(0.5, "cm")))
-    return (c1+c2+l1)
-}
 
-  shapeoverlayCircles<-function(radius1,radius2,penLoc){
-    #uses Plotrix
-    draw.circle(0,0,radius1,lwd=2,border="red")
-    draw.circle(radius1+radius2,0,abs(radius2),lwd=2,border="red")
-    # lines(x=c(radius1+radius2,radius1+radius2-penLoc*sign(radius2)),y=c(0,0),lwd=2,col="green")
-    Arrows(x0=radius1+radius2,x1=radius1+radius2-penLoc*sign(radius2),
-           y0=0,y1=0,lwd=2,col="blue",arr.adj=1)  
-  }
+# ggoverlayCircles<-function(base.gg,radius1,radius2,penLoc,penColor){
+#   
+#   c1<-geom_path(data=circle(0,0,radius1),aes(x,y),lwd=2,na.rm=TRUE)
+#   c2<-geom_path(data=circle(radius1+radius2,0,abs(radius2)),aes(x,y),lwd=2,na.rm=TRUE)
+#   l1<- geom_segment(x = (radius1 + radius2), 
+#                     y = 0, 
+#                     xend = (radius1 + radius2 - penLoc*sign(radius2)), 
+#                     yend = 0,
+#                     arrow = arrow(length = unit(0.5, "cm")),
+#                     color=penColor,lwd=2)
+#   
+#   return (base.gg + c1+c2+l1)
+# }
+
+ggoverlayCircles<-function(base.gg,radius1,radius2,px,py,cx,cy,penColor){
+  
+  c1<-geom_path(data=circle(0,0,radius1),aes(x,y),lwd=2,na.rm=TRUE)
+  c2<-geom_path(data=circle(cx,cy,abs(radius2)),aes(x,y),lwd=2,na.rm=TRUE)
+  l1<- geom_segment(x = cx, 
+                    y = cy, 
+                    xend = px, 
+                    yend = py,
+                    arrow = arrow(length = unit(0.5, "cm")),
+                    color=penColor,lwd=2)
+  
+  return (base.gg + c1+c2+l1)
+}
 
   hypoTrochoid <-function(r1=100,r2=-10,penLoc=10,theta=defaultTheta)  {
     #cyclic Trochoid
     #http://en.wikipedia.org/wiki/Hypotrochoid
-    # in this convention we denote hypo- byt a negative r2 so flip this sign for the usual formulation
+    # in the user interface we denote hypotrochoid by a negative r2 so flip this sign for the usual formulation
     r2 = -r2
     
-    x <-(r1-r2)*cos(theta) + penLoc*cos((r1-r2)/r2*theta)
-    y <-(r1-r2)*sin(theta) - penLoc*sin((r1-r2)/r2*theta)
-    return(data.frame(x,y))
+    #cx,cy is center of moving ring, x,y is point to plot
+    cx <-(r1-r2)*cos(theta)
+    x <- cx + penLoc*cos((r1-r2)/r2*theta)
+    cy <-(r1-r2)*sin(theta)
+    y <- cy - penLoc*sin((r1-r2)/r2*theta)
+    return(data.frame(x,y,cx,cy))
   }
   
   epiTrochoid <-function(r1=100,r2=10,penLoc=10,theta=defaultTheta)  {
     #cyclic Trochoid
-    #http://en.wikipedia.org/wiki/Hypotrochoid
+    #http://en.wikipedia.org/wiki/epitrochoid
 
-    x <-(r1+r2)*cos(theta)-penLoc*cos((r1+r2)/r2*theta)
-    y <-(r1+r2)*sin(theta)-penLoc*sin((r1+r2)/r2*theta)
-    return(data.frame(x,y))
+    cx <-(r1+r2)*cos(theta)
+    x<- cx - penLoc*cos((r1+r2)/r2*theta)
+    cy <-(r1+r2)*sin(theta)
+    y<- cy - penLoc*sin((r1+r2)/r2*theta)
+    return(data.frame(x,y,cx,cy))
   }
-  
-  plotRandomcTrochoid<-function() {
-    radius1=round(runif(1,1,100))
-    radius2=round(runif(1,-50,50))
-    penLoc=round(radius2*runif(1,0,2))
-    dat<-cTrochoid(radius1,radius2,penLoc,defaultTheta)
-    plot(dat$x,dat$y,type='l',xlab='x',ylab='y')
-    sub=paste('Big Circle=',radius1,'Small Circle=',radius2,'Pen Point=',penLoc)
-    if (radius2>0)
-      main='Epitrochoid'
-    else
-      main='Hypotrochoid'
-    
-    title(main=main,sub=sub)
-  }
-  
-  
-  plotcTrochoid<-function(radius1=100,radius2=10,penLoc=5,overlay=TRUE) {
-    if (radius2>0)
-      dat<-epiTrochoid(radius1,radius2,penLoc,defaultTheta)
-    else
-      dat<-hypoTrochoid(radius1,radius2,penLoc,defaultTheta)
-    plotRange=c(-max(abs(dat)),max(abs(dat)))
-    plot(dat$x,dat$y,type='l',xlab='x',ylab='y',asp=1,xlim=plotRange,ylim=plotRange)
-    sub=paste('Big Circle=',radius1,'Small Circle=',radius2,'Pen Point=',penLoc)
-    if (radius2>0)
-      main='Epitrochoid'
-    else
-      main='Hypotrochoid'
-    
-    title(main=main,sub=sub)
-    if (overlay) overlayCircles(radius1,radius2,penLoc)
-  }
-  
-  ggplotcTrochoid<-function(radius1=100,radius2=10,penLoc=5,overlay=TRUE) {
-    if (radius2>0)
-      dat<-epiTrochoid(radius1,radius2,penLoc,defaultTheta)
-    else
-      dat<-hypoTrochoid(radius1,radius2,penLoc,defaultTheta)
-    plotRange=c(-max(abs(dat)),max(abs(dat)))
-    t.plot <- ggplot(data=dat,aes(x,y))+geom_path()+xlim(plotRange)+ylim(plotRange)
-    #,xlab='x',ylab='y',asp=1,xlim=plotRange,ylim=plotRange)
-    sub=paste('Big Circle=',radius1,'Small Circle=',radius2,'Pen Point=',penLoc)
-    if (radius2>0)
-      main='Epitrochoid'
-    else
-      main='Hypotrochoid'
-    
-    title(main=main,sub=sub)
-    if (overlay) ggoverlayCircles(radius1,radius2,penLoc)
-    # now plot it
-    print(t.plot)
-  }
-  
   
 
-animatecTrochoid<-function(radius1=100,radius2=20,penLoc=5,point=length(defaultTheta),overlay=TRUE) {
-
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+compute_CTrochoid<-function(radius1=100,radius2=20, penLoc=5) {
+  
+  #figuretype is not used yet
+  
+  
+  if (radius2>0) {
+    # rotating ring goes outside stationary ring
+    figureType ='Epitrochoid'
+    if (radius1==radius2)  figureType="Epicycloid (stationary Ring=Rotating Ring)"
+    if (penLoc==radius2)  figureType="Limaçon (Pen Size = Rotating Ring)"      
+    if (penLoc==0)  figureType="Circle (Pen Size = 0)"      
+    dat<-epiTrochoid(radius1,radius2,penLoc,defaultTheta)
+  }
+  else {
+    # rotating ring goes inside stationary ring
+    figureType = 'Hypotrochoid'
+    if (radius1==radius2)  figureType="Ellipse (stationary Ring = 2 * Rotating Ring)"
+    if (penLoc==radius2)  figureType="Hypocycloid (Pen Size = Rotating Ring)"      
     
-    if (radius2>0)
-      dat<-epiTrochoid(radius1,radius2,penLoc,defaultTheta)
-    else
-      dat<-hypoTrochoid(radius1,radius2,penLoc,defaultTheta)
-    if (radius2>0)
-      main='Epitrochoid'
-    else
-      main='Hypotrochoid'
-    plotRange=c(-max(abs(dat)),max(abs(dat)))
-    # plot(dat[1,],type='l',xlab='x',ylab='y',asp=1,xlim=plotRange,ylim=plotRange)
-    sub=paste('Big Circle=',radius1,'Small Circle=',radius2,'Pen Location=',penLoc)
-    # title(main=main,sub=sub)
-    if (debug) print(paste(point))
-    t.plot<-ggplot(data=dat[1:point,],aes(x=x,y=y))+geom_path()+xlim(plotRange)+ylim(plotRange)
-    print(t.plot)
-    #plot(dat[1:point,],type='l',xlab='x',ylab='y',asp=1,xlim=plotRange,ylim=plotRange)
-    if (overlay) 
-      print(t.plot+ggoverlayCircles(radius1,radius2,penLoc))
-    else
-      print(t.plot)
+    dat<-hypoTrochoid(radius1,radius2,penLoc,defaultTheta)
   } 
   
+  
+  return (dat)
+} #-------------------------------------------------------------------------------------------
+ggplotcTrochoid<-function(dat,radius1,radius2,point=defaultPoints,
+                          overlay=TRUE,penColor="black",zoom=FALSE) {
+
+      
+    # zoom might clip ring visibility but make figure fill plot area
+    if (zoom)
+      maxRange= max(abs(dat))
+    else
+      maxRange=max(abs(dat))*1.3
+    
+    plotRange=c(-maxRange,maxRange)
+    
+    t.plot<-ggplot(data=dat[1:point,],aes(x=x,y=y))+geom_path(color=penColor)+xlim(plotRange)+ylim(plotRange)
+    #plot rotating ring and 'pen' for last point
+    if (overlay) t.plot <- ggoverlayCircles(t.plot,radius1,radius2,
+                                            px =dat[point,]$x,
+                                            py =dat[point,]$y,
+                                            cx =dat[point,]$cx,
+                                            cy =dat[point,]$cy,
+                                            penColor)
+    print(t.plot)
+  } 
+# --------------------------------------------------------------------------  
+# precompute all points of trochoid and center of rotating ring.
+# Don't recompute unless these inputs change
+trochoidPoints <- reactive({    compute_CTrochoid(radius1=input$ring1,
+                                                  radius2=input$ring2,
+                                                  penLoc=input$penLoc
+                                                  )  
+})
+
+# aniSpeed<- reactive({ 
+#   switch(input$speed,
+#           "slow"=defaultPoints/20,
+#           "medium"=defaultPoints/50,
+#           "fast"=defaultPoints/100)
+# })
+
+#-------------------------------------------------------------------
+#plot the figure
+# output$speed <- reactive({aniSpeed()})
+
+
 output$distPlot <- renderPlot({
  
-    animatecTrochoid(radius1=input$ring1,
-                     radius2=input$ring2,
-                     penLoc=input$penLoc,
-                     point=input$point,
-                     overlay=input$showRings)
+    ggplotcTrochoid(trochoidPoints(),
+                    radius1=input$ring1,
+                    radius2=input$ring2,
+                    point=input$point,
+                    overlay=input$showRings,
+                    penColor=input$penColor,
+                    zoom=input$zoom)
   })
 
 })
--- ui.R
+++ ui.R
@@ -26,22 +26,46 @@ shinyUI(fluidPage(
                   "Unit Size of Rotating Ring:",
                   min = -100,
                   max = 120,
-                  value = 10),
+                  value = -30),
       sliderInput("penLoc",
                   "Pen relative to radius of ring 2:",
-                  min = 1,
+                  min = 0,
                   max = 50,
                   value = 20),
+      radioButtons("penColor","Pen Color:",
+                   c("Red"="red",
+                     "Green" = "darkgreen",
+                     "Blue" = "blue")),
 
 
-      checkboxInput("showRings","Show Rings?",TRUE),
+      checkboxInput("showRings","Show Rings?",TRUE),      
+      checkboxInput("zoom","Zoom? (won't always make a difference)",FALSE),
+
       sliderInput("point","Number of points drawn",min=2,max=points,value=2,step=5,
-                  animate=animationOptions(interval=points/30))
+                  animate=animationOptions(interval=points/60))
       
+#       selectInput("speed", "Animation Speed", c("slow", "medium", "fast")),
+#       
+#       conditionalPanel(
+#         condition="input.speed == slow",
+#           sliderInput("point","Number of points drawn",min=2,max=points,value=2,step=5,
+#                   animate=animationOptions(interval=points/20))
+#       )
+#        conditionalPanel(
+#         condition= "input.speed == medium",
+#         sliderInput("point","Number of points drawn",min=2,max=points,value=2,step=5,
+#                     animate=animationOptions(interval=points/50))
+#       )
+#       conditionalPanel(
+#         condition= "input.speed == fast",
+#         sliderInput("point","Number of points drawn",min=2,max=points,value=2,step=5,
+#                     animate=animationOptions(interval=points/100))
+#       )        
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
+      textOutput("speed"),
       plotOutput("distPlot",width="400px",height="400px")
     )
   )
